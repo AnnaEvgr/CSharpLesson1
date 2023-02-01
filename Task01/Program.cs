349c88834691ab97e2606848bd7462d5fbf91b2b
AnnaEvgrafova <eaa79evgrafova@yandex.ru>
2023-02-01 21:35:20 +0500

    Урок 1

---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+System.Console.WriteLine("Введите первое число");
+String? number = Console.ReadLine();
+int a = Convert.ToInt32(number);
+System.Console.WriteLine("Введите второе число");
+String? number2 = Console.ReadLine();
+int b = Convert.ToInt32(number2);
+if (a < b)
+{
+    System.Console.WriteLine($"Максимальное число {b}");
+}
+else System.Console.WriteLine($"Максимальное число {a}");
---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+System.Console.WriteLine("Введите первое число");
+String? number = Console.ReadLine();
+int a = Convert.ToInt32(number);
+System.Console.WriteLine("Введите второе число");
+String? number2 = Console.ReadLine();
+int b = Convert.ToInt32(number2);
+System.Console.WriteLine("Введите третье число");
+String? number3 = Console.ReadLine();
+int c = Convert.ToInt32(number3);
+if ((a > b) && (a > c))
+{
+    System.Console.WriteLine($"Max {a}");
+}
+else
+if (b > c)
+{
+    System.Console.WriteLine($"Max {b}");
+}
+else System.Console.WriteLine($"Max {c}");
